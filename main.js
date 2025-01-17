633edd27a189ebcdf774d171d25a76e344c4f40a
Aplha101 <dhruvbeastboy2020@gmail.com>
2025-01-17 18:39:54 +0530

    update hah

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+// Create an audio object
+const audio = new Audio('/UEditor_Untitled_Project_V1_1.mp3');
+
+// Set audio properties
+audio.loop = true; // Enable looping
+
+// Function to play audio on mousemove
+function playAudioOnMouseMove() {
+  audio.play().then(() => {
+    console.log("Audio is playing");
+    // Remove the event listener once the audio starts playing
+    document.removeEventListener('mousemove', playAudioOnMouseMove);
+  }).catch((error) => {
+    console.error("Audio play failed:", error);
+  });
+}
+
+// Add the event listener
+document.addEventListener('mousemove', playAudioOnMouseMove);
